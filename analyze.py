96e5672d0f0452f6f3be9dd56fb0f13c9f44f854
JosePereiraUA <jose.manuel.pereira@ua.pt>
2019-12-12 11:02:32 -0500

    Refactored analyze script

--- analyze.py
+++ analyze.py
@@ -2,7 +2,8 @@ import argparse
 from pyrosetta import *
 from ze_utils.pyrosetta_tools import *
 from pyrosetta.rosetta.core.scoring import *
-from pyrosetta.rosetta.core.select.residue_selector import *
+from pyrosetta.rosetta.core.select.residue_selector import \
+    ChainSelector, NeighborhoodResidueSelector
 from pyrosetta.rosetta.core.simple_metrics.metrics import \
 	InteractionEnergyMetric, SasaMetric
 
@@ -39,48 +40,13 @@ from pyrosetta.rosetta.core.simple_metrics.metrics import \
 #                  Rosetta Common Latest Documentation:
 # https://www.rosettacommons.org/docs/latest (*add the topic here*)
 
-class Data():
-    """
-    Save relevant Data from the PDB Pose, gathered from SimpleMetrics.
-    """
-    def __init__(self, filename):
-        self.filename           = filename
-        self.total_energy       = 0.0
-        self.hbne               = 0.0
-        self.interaction        = None
-        self.res_e              = {}
-        self.sasa               = {}
-
-    def __str__(self):
-        return self.as_string()
-
-    def as_string(self):
-        """
-        Print the Data content as a string.
-        """
-        s = ""
-        s += "%40s\n" % (self.filename)
-        s += "%40s: %12.4f\n" % ("Total energy", self.total_energy)
-        if not self.interaction == None:
-            s += "%40s: %12.4f\n" % ("Interaction energy", self.interaction)
-        s += "%40s: %12.4f\n" % ("Hydrogen bonding network energy", self.hbne)
-        s += "%40s:\n" % ("Solvent Accessible Surface Area (SASA)")
-        for selection in self.sasa:
-            s += "%39s:  %12.4f\n" % (selection, self.sasa[selection])
-        s += "%40s:\n" % ("Individual residue total energies")
-        for sel in self.res_e:
-            s += "%39s:\n" % (sel)
-            for (_, i, e) in self.res_e[sel]:
-                s += "%38d    %12.4f\n" % (i, e)
-        return s
-
 
 class DEFAULT:
     """
     Define defaults for the script. Values can be modified using arguments.
     """
-    selections  = "selections.json"
-    output_file = "analyze.dat"
+    cut_off = 9.0
+    chain   = 'C'
 
 
 def validate_arguments(args):
@@ -89,8 +55,6 @@ def validate_arguments(args):
     """
     if not args.input_file[-4:] == ".pdb":
         exit("ERROR: Input file should be in PDB format")
-    if not args.selections[-5:] == ".json":
-        exit("ERROR: Selections file should be in JSON format")
 
 
 if __name__ == "__main__":
@@ -98,12 +62,12 @@ if __name__ == "__main__":
         outputing intra-structure metrics.""")
     parser.add_argument('input_file', metavar='INPUT', type=str,
         help='The input PDB file')
-    parser.add_argument('-s', '--selections', metavar='', type=str,
-        help='Selections JSON file (Default: %s)' % (DEFAULT.selections),
-        default = DEFAULT.selections)
-    parser.add_argument('-o', '--output_file', metavar='', type=str,
-        help='Output file (Default: %s)' % (DEFAULT.output_file),
-        default = DEFAULT.output_file)
+    parser.add_argument('-co', '--cutoff', metavar='', type=float,
+        help='Interface identification cut-off in Å (Default: %5.1f)' \
+            % (DEFAULT.cut_off), default = DEFAULT.cut_off)
+    parser.add_argument('-c', '--chain', metavar='', type=str,
+        help='Chain defined as index (Default: %s)' \
+            % (DEFAULT.chain), default = DEFAULT.chain)
 
     args = parser.parse_args()
     validate_arguments(args)
@@ -111,38 +75,45 @@ if __name__ == "__main__":
 
     pose           = pose_from_pdb(args.input_file)
     score_function = get_fa_scorefxn()
-    selections     = load_selections_from_json_file(args.selections, pose)
-    data           = Data(args.input_file)
 
     # 1. Total Energy
-    data.total_energy = score_function(pose)
+    # This needs to be the first step, as the score needs to be scored, for
+    # example, for the InteractionEnergyMetric calculation.
+    total_energy = score_function(pose)
 
-    # 2. Individual Residues Energy per Selection
-    for selection in selections:
-        if not selections[selection] == None:
-            data.res_e[selection] = get_residue_energies_from_selector(
-                selections[selection],
-                pose,
-                verbose = False)
 
     # 3. Interaction Energy Metric
-    # This should only be calculated if the selections has a ligand.
-    if not (selections["lig"] == None or selections["interface"] == None):
-        ie = InteractionEnergyMetric(selections["lig"], selections["interface"])
-        data.interaction = ie.calculate(pose)
+    # This should only be calculated if a chain C is present on the pose.
+    ligand_residues, interface_residues = [], []
+    interaction_energy = None
+    ligand = ChainSelector(args.chain)
+    interface = NeighborhoodResidueSelector(ligand, args.cutoff,
+        include_focus_in_subset = False)
+    if len(get_residues_from_selector(ligand, pose)) > 0:
+        interaction_energy_metric = InteractionEnergyMetric(ligand, interface)
+        interaction_energy        = interaction_energy_metric.calculate(pose)
 
-    # 4. Solvent Acessible Surface Area (SASA) Metric
-    sasa_metric = SasaMetric()
-    for selection in selections:
-        if not selections[selection] == None:
-            sasa_metric.set_residue_selector(selections[selection])
-            data.sasa[selection] = sasa_metric.calculate(pose)
+        # 2. Individual Residues Energy per Selection
+        ligand_residues    = get_residue_energies_from_selector(ligand, pose)
+        interface_residues = get_residue_energies_from_selector(interface, pose)
 
-    # 5. Hydrogen Bonding Network Energy
+    # 4. Hydrogen Bonding Network Energy
+    hydrogen_bonding_energy = 0.0
     score_terms = [hbond_sr_bb, hbond_lr_bb, hbond_bb_sc, hbond_sc]
     for score_term in score_terms:
-        data.hbne += pose.energies().total_energies()[score_term]
+        hydrogen_bonding_energy += pose.energies().total_energies()[score_term]
 
-    with open(args.output_file, "w") as file_out:
-        file_out.write(data.as_string())
-    print("\n\nDone!")
+
+    print("\n\n%30s %10s\n %s" % ("Metric", "Value", "-"*40))
+    print("%30s %10.3f" % ("Total energy", total_energy))
+    print("%30s %10.3f" % ("Hydrogen bonding energy", hydrogen_bonding_energy))
+    if not interaction_energy == None:
+        print("%30s %10.3f" % ("Interaction energy", interaction_energy))
+    if len(ligand_residues) > 0:
+        print("\n\n%30s %10s\n %s" % ("Ligand residue index", "Energy", "-"*40))
+        for (_, index, energy) in ligand_residues:
+            print("%30s %10.3f" % (index, energy))
+    if len(interface_residues) > 0:
+        print("\n\n%30s %10s\n %s" % ("Interface residue index", "Energy", "-"*40))
+        for (_, index, energy) in interface_residues:
+            print("%30s %10.3f" % (index, energy))
