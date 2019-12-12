30fd2bdc9b180aedbd9319213fd245934788306b
JosePereiraUA <jose.manuel.pereira@ua.pt>
2019-12-12 15:41:09 -0500

    Added verify_launch_failed_request_failed

--- ze_utils/common.py
+++ ze_utils/common.py
@@ -5,21 +5,29 @@ from os.path import expanduser as expusr
 #
 #            \\ INITIALLY CREATED BY JOSE PEREIRA, 2019 \\
 #
-# Functions included:
+#    Functions included:
+#
 # 1) get_number_of_jobs_in_slurm_queue
 #    Returns the numbers of SLURM jobs in the current user's queue
 #
-# 2) progress_bar
+# 2) verify_launch_failed_request_failed
+#    Automatically recovers jobs from the '(launch failed requeued held)' error
+#
+# 3) progress_bar
 #    Returns a progress bar string with a set length and set to the given value
 #
-# 3) read_matrix_from_txt_file
+# 4) read_matrix_from_txt_file
 #    Returns a BLOSUM62 matrix and its entries
 #
-# 4) blosum62
+# 5) blosum62
 #    Use a BLOSUM62 matrix and entries to calculate the score between 2 strings
 #
-# 5) overwrite_dir
+# 6) overwrite_dir
 #    Create a new directory, overwritting any existing data
+#
+# 7) load_data_from_fasc_file
+#    Returns an array with all Dictionary entries found in the .fasc file.
+
 
 def get_number_of_jobs_in_slurm_queue(user):
     """
@@ -47,6 +55,27 @@ def get_number_of_jobs_in_slurm_queue(user):
             number_of_jobs_in_slurm_queue += int(elem1[2]) - int(elem1[1])
 
     return number_of_jobs_in_slurm_queue
+
+
+def verify_launch_failed_request_failed(user):
+    """
+    Verifies the 'user's squeue, searching for '(launch failed requeued held)'
+    errors, releasing the jobs.
+    """
+
+    import os
+    import subprocess
+
+    squeue = subprocess.check_output(["squeue", "-u", user]).splitlines()[1:]
+    failed_jobs = []
+    for line in squeue:
+        elem = [str(e)[2:-1] for e in line.split()]
+        if " ".join(elem[7:]) == '(launch failed requeued held)':
+            failed_jobs.append(elem[0])
+
+    if len(failed_jobs) > 0:
+        print("Releasing held jobs: %s" % (" ".join(failed_jobs)))
+        os.system("scontrol release %s" % (" ".join(failed_jobs)))
 
 
 def progress_bar(current, maximum, length, fill = "#", empty = "-"):
@@ -111,3 +140,21 @@ def overwrite_dir(path):
         print(" >> [ WARNING ] Overwritting directory %s" % (path))
         shutil.rmtree(path)
     os.mkdir(path)
+
+
+def load_data_from_fasc_file(file_path):
+    """
+    Returns an array with all Dictionary entries found in the .fasc file.
+    """
+    json_path = "["
+    with open(file_path, "r") as file_in:
+        for line in file_in:
+            json_path += line[:-1]
+            json_path += ","
+    json_path = json_path[:-1] + "]"
+    
+    try:
+        return json.loads(json_path)
+    except:
+        import json
+        return json.loads(json_path)
